32efb042c5d5081523f546b57e6eb89f9389c35f
Guido Perman <guidoyaelperman@gmail.com>
2025-12-26 13:41:13 -0300

    feat: Add dashboard page and signal forms component, configure HTTP client, and update app routing and layout.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./pages/dashboard/dashboard').then(c => c.Dashboard),
+        children: [
+            {
+                path: 'signal-forms',
+                loadComponent: () => import('./components/signal-forms/signal-forms').then(c => c.SignalForms)
+            },
+            {
+                path: '',
+                redirectTo: 'signal-forms',
+                pathMatch: 'full'
+            }
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    }
+];
